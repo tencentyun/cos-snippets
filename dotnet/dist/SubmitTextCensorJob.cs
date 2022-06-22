59dea2324870549607dfb4d3a0b28d6dd294eb78
shawnnqin <shawnnqin@tencent.com>
2022-06-22 15:37:48 +0800

    增加文本同步审核的demo

--- dotnet/dist/SubmitTextCensorJob.cs
+++ dotnet/dist/SubmitTextCensorJob.cs
@@ -1,6 +1,7 @@
 using COSXML.Model.CI;
 using COSXML.Auth;
 using System;
+using System.Text;
 using System.Threading;
 using COSXML;
 
@@ -30,7 +31,7 @@ namespace COSSnippet
         //.cssg-snippet-body-start:[SubmitAudioCensorJob]
         // 存储桶名称，此处填入格式必须为 bucketname-APPID, 其中 APPID 获取参考 https://console.cloud.tencent.com/developer
         string bucket = "examplebucket-1250000000"; // 注意：此操作需要 bucket 开通内容审核相关功能
-         SubmitTextCensorJobRequest request = new SubmitTextCensorJobRequest(bucket);
+        SubmitTextCensorJobRequest request = new SubmitTextCensorJobRequest(bucket);
         request.SetCensorObject("text.txt"); // 媒体文件的对象键，需要替换成桶内存在的媒体文件的对象键
         // 审核的场景类型，有效值：Porn（涉黄）、Terrorism（涉暴恐）、Politics（政治敏感）、Ads（广告），可以传入多种类型，不同类型以逗号分隔，例如：Porn,Terrorism
         request.SetDetectType("Porn,Terrorism");
@@ -79,9 +80,40 @@ namespace COSSnippet
         //.cssg-snippet-body-end
       }
 
+      /// 提交同步审核的文本审核任务
+      public void SubmitTextCensorJobSync()
+      {
+        //.cssg-snippet-body-start:[SubmitAudioCensorJob]
+        // 存储桶名称，此处填入格式必须为 bucketname-APPID, 其中 APPID 获取参考 https://console.cloud.tencent.com/developer
+        string bucket = "examplebucket-1250000000"; // 注意：此操作需要 bucket 开通内容审核相关功能
+        SubmitTextCensorJobRequest request = new SubmitTextCensorJobRequest(bucket);
+        request.SetCensorContent(Convert.ToBase64String(Encoding.UTF8.GetBytes("文本审核内容"))); // 需要审核的文本内容，注意需要base64编码后传入
+        // 审核的场景类型，有效值：Porn（涉黄）、Terrorism（涉暴恐）、Politics（政治敏感）、Ads（广告），可以传入多种类型，不同类型以逗号分隔，例如：Porn,Terrorism
+        request.SetDetectType("Porn,Terrorism");
+        // 执行请求
+        SubmitTextCensorJobsResult result = new SubmitTextCensorJobsResult();
+        try {
+            result = cosXml.SubmitTextCensorJobSync(request);
+        } catch (COSXML.CosException.CosClientException clientEx) {
+          //请求失败
+          Console.WriteLine("CosClientException: " + clientEx);
+        }
+        catch (COSXML.CosException.CosServerException serverEx) {
+          //请求失败
+          Console.WriteLine("CosServerException: " + serverEx.GetInfo());
+        }
+        Console.WriteLine(result.GetResultInfo());
+        Console.WriteLine(result.textCensorJobsResponse.JobsDetail.JobId);
+        Console.WriteLine(result.textCensorJobsResponse.JobsDetail.Result);
+        Console.WriteLine(result.textCensorJobsResponse.JobsDetail.SectionCount);
+        //.cssg-snippet-body-end
+      }
+
       static void Main(string[] args)
       {
         SubmitTextCensorJobModel m = new SubmitTextCensorJobModel();
+        /// 提交同步审核任务
+        m.SubmitTextCensorJobSync();
         /// 提交审核任务
         string JobId = m.SubmitTextCensorJob();
         Thread.Sleep(30000);
