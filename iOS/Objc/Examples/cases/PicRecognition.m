165753aa8246ef2a5a1460244e70427fe4a51e83
garenwang <garenwang@tencent.com>
2024-05-14 20:25:19 +0800

    补充示例

---
+++ iOS/Objc/Examples/cases/PicRecognition.m
@@ -0,0 +1,87 @@
+#import <XCTest/XCTest.h>
+#import <QCloudCOSXML/QCloudCOSXML.h>
+#import <QCloudCOSXML/QCloudCIPicRecognitionRequest.h>
+
+
+@interface PicRecognition : XCTestCase <QCloudSignatureProvider,
+                                               QCloudCredentailFenceQueueDelegate>
+
+@property (nonatomic) QCloudCredentailFenceQueue* credentialFenceQueue;
+
+@end
+
+@implementation PicRecognition
+
+- (void)setUp {
+    // 注册默认的 COS 服务
+    QCloudServiceConfiguration* configuration = [QCloudServiceConfiguration new];
+    configuration.appID = @"1250000000";
+    configuration.signatureProvider = self;
+    QCloudCOSXMLEndPoint* endpoint = [[QCloudCOSXMLEndPoint alloc] init];
+    endpoint.regionName = @"ap-guangzhou";//服务地域名称，可用的地域请参考注释
+    configuration.endpoint = endpoint;
+    [QCloudCOSXMLService registerDefaultCOSXMLWithConfiguration:configuration];
+    [QCloudCOSTransferMangerService registerDefaultCOSTransferMangerWithConfiguration:configuration];
+
+    // 脚手架用于获取临时密钥
+    self.credentialFenceQueue = [QCloudCredentailFenceQueue new];
+    self.credentialFenceQueue.delegate = self;
+}
+
+- (void) fenceQueue:(QCloudCredentailFenceQueue * )queue
+    requestCreatorWithContinue:(QCloudCredentailFenceQueueContinue)continueBlock
+{
+    QCloudCredential* credential = [QCloudCredential new];
+    //在这里可以同步过程从服务器获取临时签名需要的 secretID，secretKey，expiretionDate 和 token 参数
+    credential.secretID = @"COS_SECRETID";
+    credential.secretKey = @"COS_SECRETKEY";
+    credential.token = @"COS_TOKEN";
+    /*强烈建议返回服务器时间作为签名的开始时间，用来避免由于用户手机本地时间偏差过大导致的签名不正确 */
+    credential.startDate = [[[NSDateFormatter alloc] init] dateFromString:@"startTime"]; // 单位是秒
+    credential.expirationDate = [[[NSDateFormatter alloc] init] dateFromString:@"expiredTime"];
+    QCloudAuthentationV5Creator* creator = [[QCloudAuthentationV5Creator alloc]
+        initWithCredential:credential];
+    continueBlock(creator, nil);
+}
+
+- (void) signatureWithFields:(QCloudSignatureFields*)fileds
+                     request:(QCloudBizHTTPRequest*)request
+                  urlRequest:(NSMutableURLRequest*)urlRequst
+                   compelete:(QCloudHTTPAuthentationContinueBlock)continueBlock
+{
+    [self.credentialFenceQueue performAction:^(QCloudAuthentationCreator *creator,
+                                               NSError *error) {
+        if (error) {
+            continueBlock(nil, error);
+        } else {
+            QCloudSignature* signature =  [creator signatureForData:urlRequst];
+            continueBlock(signature, nil);
+        }
+    }];
+}
+
+
+- (void)PicRecognition {
+    
+    //.cssg-snippet-body-start:[objc-init-multi-upload]
+    QCloudCIPicRecognitionRequest *request = [QCloudCIPicRecognitionRequest new];
+    // 存储桶名称，由BucketName-Appid 组成，可以在COS控制台查看 https://console.cloud.tencent.com/cos5/bucket
+    request.bucket = @"bucket";
+    // 对象键，是对象在 COS 上的完整路径，如果带目录的话，格式为 "dir1/object1"
+    request.object = @"pic-recognition.png";
+
+
+    [request setFinishBlock:^(QCloudCIPicRecognitionResults *_Nullable result, NSError *_Nullable error) {
+      //result：返回的标签信息
+    }];
+
+
+    [[QCloudCOSXMLService defaultCOSXML] CIPicRecognition:request];
+
+    
+    //.cssg-snippet-body-end
+
+}
+
+
+@end
---
+++ iOS/Swift/Examples/cases/PicRecognition.swift
@@ -0,0 +1,72 @@
+import XCTest
+import QCloudCOSXML
+
+class PicRecognition: XCTestCase,QCloudSignatureProvider,QCloudCredentailFenceQueueDelegate{
+    
+    var credentialFenceQueue:QCloudCredentailFenceQueue?;
+    var uploadId : String?;
+    
+    
+    override func setUp() {
+        let config = QCloudServiceConfiguration.init();
+        config.signatureProvider = self;
+        config.appID = "1253653367";
+        let endpoint = QCloudCOSXMLEndPoint.init();
+        endpoint.regionName = "ap-guangzhou";//服务地域名称，可用的地域请参考注释
+        endpoint.useHTTPS = true;
+        config.endpoint = endpoint;
+        QCloudCOSXMLService.registerDefaultCOSXML(with: config);
+        QCloudCOSTransferMangerService.registerDefaultCOSTransferManger(with: config);
+        
+        // 脚手架用于获取临时密钥
+        self.credentialFenceQueue = QCloudCredentailFenceQueue();
+        self.credentialFenceQueue?.delegate = self;
+    }
+    
+    func fenceQueue(_ queue: QCloudCredentailFenceQueue!,
+                    requestCreatorWithContinue continueBlock: QCloudCredentailFenceQueueContinue!) {
+        let cre = QCloudCredential.init();
+        //在这里可以同步过程从服务器获取临时签名需要的 secretID，secretKey，expiretionDate 和 token 参数
+        cre.secretID = "COS_SECRETID";
+        cre.secretKey = "COS_SECRETKEY";
+        cre.token = "COS_TOKEN";
+        /*强烈建议返回服务器时间作为签名的开始时间，用来避免由于用户手机本地时间偏差过大导致的签名不正确 */
+        cre.startDate = DateFormatter().date(from: "startTime"); // 单位是秒
+        cre.expirationDate = DateFormatter().date(from: "expiredTime");
+        let auth = QCloudAuthentationV5Creator.init(credential: cre);
+        continueBlock(auth,nil);
+    }
+    
+    func signature(with fileds: QCloudSignatureFields!,
+                   request: QCloudBizHTTPRequest!,
+                   urlRequest urlRequst: NSMutableURLRequest!,
+                   compelete continueBlock: QCloudHTTPAuthentationContinueBlock!) {
+        self.credentialFenceQueue?.performAction({ (creator, error) in
+            if error != nil {
+                continueBlock(nil,error!);
+            }else{
+                let signature = creator?.signature(forData: urlRequst);
+                continueBlock(signature,nil);
+            }
+        })
+    }
+
+    /**
+     * 获取工作流实例详情
+     */
+    func picRecognition() {
+        let put = QCloudCIPicRecognitionRequest();
+        // 对象键，是对象在 COS 上的完整路径，如果带目录的话，格式为 "dir1/object1"
+        put.object = "exampleobject";
+        // 存储桶名称，由BucketName-Appid 组成，可以在COS控制台查看 https://console.cloud.tencent.com/cos5/bucket
+
+        put.bucket = "examplebucket-1250000000";
+        put.setFinish { (outoutObject, error) in
+            //outoutObject
+        };
+        QCloudCOSXMLService.defaultCOSXML().ciPicRecognition(put);
+
+
+    }
+}
+
