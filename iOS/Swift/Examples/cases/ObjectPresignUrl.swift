81693fb6bc9c9c42c5d3f16f0712cccda74821c1
garen <1185742894@qq.com>
2024-07-05 14:36:48 +0800

    Update ObjectPresignUrl.swift

--- iOS/Swift/Examples/cases/ObjectPresignUrl.swift
+++ iOS/Swift/Examples/cases/ObjectPresignUrl.swift
@@ -53,25 +53,42 @@ class ObjectPresignUrl: XCTestCase,QCloudSignatureProvider,QCloudCredentailFence
     // 获取预签名下载链接
     func getPresignDownloadUrl() {
         //.cssg-snippet-body-start:[swift-get-presign-download-url]
-        let getPresign  = QCloudGetPresignedURLRequest.init();
+        QCloudGetPresignedURLRequest* getPresignedURLRequest = [[QCloudGetPresignedURLRequest alloc] init];
         
-        // 存储桶名称，格式为 BucketName-APPID
-        getPresign.bucket = "examplebucket-1250000000" ;
         
-        // 使用预签名 URL 的请求的 HTTP 方法。有效值（大小写敏感）为：
-        // @"GET"、@"PUT"、@"POST"、@"DELETE"
-        getPresign.httpMethod = "GET";
+        // 存储桶名称，由BucketName-Appid 组成，可以在COS控制台查看 https://console.cloud.tencent.com/cos5/bucket
+        getPresignedURLRequest.bucket = @"examplebucket-1250000000";
         
-        // 对象键，是对象在 COS 上的完整路径，如果带目录的话，格式为 "dir1/object1"
-        getPresign.object = "exampleobject";
-        getPresign.setFinish { (result, error) in
-            if let result = result {
-                let url = result.presienedURL
-            } else {
-                print(error!);
-            }
-        }
-        QCloudCOSXMLService.defaultCOSXML().getPresignedURL(getPresign);
+        
+        // 使用预签名 URL 请求的 HTTP 方法。有效值（大小写敏感）为：@"GET"、@"PUT"、@"POST"、@"DELETE"
+        getPresignedURLRequest.HTTPMethod = @"PUT";
+        
+        
+        // 获取预签名函数，默认签入Header Host；您也可以选择不签入Header Host，但可能导致请求失败或安全漏洞
+        getPresignedURLRequest.signHost = YES;
+        
+        
+        // http 请求参数，传入的请求参数需与实际请求相同，能够防止用户篡改此HTTP请求的参数
+        getPresignedURLRequest.requestParameters = @{@"param1":@"value1",@"param1":@"value1"};
+        
+        
+        // http 请求头部，传入的请求头部需包含在实际请求中，能够防止用户篡改签入此处的HTTP请求头部
+        getPresignedURLRequest.requestHeaders = @{@"param1":@"value1",@"param1":@"value1"};
+        
+        
+        // 对象键，是对象在 COS 上的完整路径，如果带目录的话，格式为 "video/xxx/movie.mp4"
+        getPresignedURLRequest.object = @"exampleobject";
+        
+        
+        [getPresignedURLRequest setFinishBlock:^(QCloudGetPresignedURLResult * _Nonnull result,
+                                                 NSError * _Nonnull error) {
+              // 预签名 URL
+              NSString* presignedURL = result.presienedURL;
+        }];
+        
+        
+        [[QCloudCOSXMLService defaultCOSXML] getPresignedURL:getPresignedURLRequest];
+
         
         //.cssg-snippet-body-end
     }
@@ -81,23 +98,40 @@ class ObjectPresignUrl: XCTestCase,QCloudSignatureProvider,QCloudCredentailFence
         //.cssg-snippet-body-start:[swift-get-presign-upload-url]
         let getPresign  = QCloudGetPresignedURLRequest.init();
         
-        // 存储桶名称，格式为 BucketName-APPID
+        
+        // 存储桶名称，由BucketName-Appid 组成，可以在COS控制台查看 https://console.cloud.tencent.com/cos5/bucket
         getPresign.bucket = "examplebucket-1250000000" ;
         
-        // 使用预签名 URL 的请求的 HTTP 方法。有效值（大小写敏感）为：
+        
+        // 使用预签名 URL 请求的 HTTP 方法。有效值（大小写敏感）为：
         // @"GET"、@"PUT"、@"POST"、@"DELETE"
         getPresign.httpMethod = "PUT";
         
-        // 对象键，是对象在 COS 上的完整路径，如果带目录的话，格式为 "dir1/object1"
+        
+        // 获取预签名函数，默认签入Header Host；您也可以选择不签入Header Host，但可能导致请求失败或安全漏洞
+        getPresign.signHost = YES;
+        
+        
+        // http 请求参数，传入的请求参数需与实际请求相同，能够防止用户篡改此HTTP请求的参数
+        getPresign.requestParameters = {"param1":"value1","param1":"value1"};
+        
+        
+        // http 请求头部，传入的请求头部需包含在实际请求中，能够防止用户篡改签入此处的HTTP请求头部
+        getPresign.requestHeaders = {"param1":"value1","param1":"value1"};
+        
+        
+        // 对象键，是对象在 COS 上的完整路径，如果带目录的话，格式为 "video/xxx/movie.mp4"
         getPresign.object = "exampleobject";
         getPresign.setFinish { (result, error) in
             if let result = result {
+                // 预签名 URL
                 let url = result.presienedURL
             } else {
                 print(error!);
             }
         }
         QCloudCOSXMLService.defaultCOSXML().getPresignedURL(getPresign);
+
         
         //.cssg-snippet-body-end
     }
